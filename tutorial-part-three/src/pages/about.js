8b2b179fb05d94a569cdcb6c6e41459efae54d9d
Nobuyuki Takahiro <studio.quad9@gmail.com>
2020-05-26 09:42:34 +0900

    learn layout

---
+++ tutorial-part-three/src/pages/about.js
@@ -0,0 +1,13 @@
+import React from "react"
+import Layout from "../components/layout"
+
+const About = () => {
+  return (
+    <Layout>
+      <h1>About Me</h1>
+      <p>I’m good enough, I’m smart enough, and gosh darn it, people like me!</p>
+    </Layout>
+  )
+}
+
+export default About

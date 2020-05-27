c40249f149f2ecb8d7d15a16c7210635638e00e4
Nobuyuki Takahiro <studio.quad9@gmail.com>
2020-05-27 16:29:45 +0900

    way to learn assets, css, layout

--- next/nextjs-blog/pages/_app.js
+++ next/nextjs-blog/pages/_app.js
@@ -1,4 +1,10 @@
+import "../styles/reset.css"
 import "../styles/global.css"
+
+// 『_app.js』で『グローバルCSS』を追加する。
+// 実際のファイルは『stylesデレクトリ』へ。
+// ブラウザに依存しているcssを一旦リセットしてから
+// 文字組を中心に全体のスタイリングをする。
 
 export default function App({ Component, pageProps }) {
   return (
